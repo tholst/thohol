6575d73fbfeac39941abc30f1f6ffff1f2e1abb7
Thomas Holst <2027794+tholst@users.noreply.github.com>
2019-11-13 02:40:50 +0100

    set up build env for react app

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+function App() { 
+    return (
+        <h1>There is nothing here yet.</h1>
+    );
+}
+
+export default App;
---
+++ webpack.config.prod.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "production";
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "production",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "build")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: ["babel-loader", "eslint-loader"]
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    resolve: { extensions: ["*", ".js", ".jsx"] },
+    devServer: {
+        stats: "minimal"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+            // favicon: "src/favicon.ico"
+        })
+    ]
+};
