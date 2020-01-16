204270e251c2d9179f1085856c447fefbab61fcd
Thomas Holst <2027794+tholst@users.noreply.github.com>
2020-01-16 04:01:53 +0100

    header title -> Thomas' Notes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import { NavLink } from "react-router-dom";
 const Header = () => (
     <header className="header hbox">
         <div className="vbox flexitem">
-            <div className="header-title flexitem">Thomas Holst</div>
-            <div className="header-subtitle flexitem">keeps notes</div>
+            <div className="header-title flexitem">{"Thomas'"}</div>
+            <div className="header-subtitle flexitem">{"Notes"}</div>
         </div>
         <nav className="header-nav flexitem">
             <ul className="nolist">
