3b2dc9b165c6a7517bb61daaa4f06667cd0b478b
Thomas Holst <2027794+tholst@users.noreply.github.com>
2020-01-19 01:26:11 +0100

    added about page and footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,23 +2,29 @@ import React from "react";
 import "./App.css";
 import ArticlesList from "./ArticleList";
 import Header from "./Header";
+import Footer from "./Footer";
+import About from "./About.md";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
     return (
-        <div className="vbox">
+        <div className="vbox app">
             {/* <h1>There is nothing here yet.</h1> */}
-            <Header className="flexitem" />
-            <div className="content">
+            <Header className="flexitem-fix" />
+            <div className="content flexitem">
                 <Switch>
                     <Route path="/posts">
                         <ArticlesList />
+                    </Route>
+                    <Route path="/about">
+                        <About />
                     </Route>
                     <Route path="/">
                         <Redirect to="/posts" />
                     </Route>
                 </Switch>
             </div>
+            <Footer className="flexitem-fix" />
         </div>
     );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 const Header = () => (
     <header className="header hbox">
-        <div className="vbox flexitem">
-            <div className="header-title flexitem">{"Thomas'"}</div>
-            <div className="header-subtitle flexitem">{"Notes"}</div>
-        </div>
+        <Link to="/posts" className="noLinkStyling">
+            <div className="vbox flexitem">
+                <div className="header-title flexitem">{"Thomas'"}</div>
+                <div className="header-subtitle flexitem">{"Notes"}</div>
+            </div>
+        </Link>
         <nav className="header-nav flexitem">
             <ul className="nolist">
                 <li className="">
