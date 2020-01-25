982c33578a566d13dc929414a63fb8e8b175a280
Thomas Holst <2027794+tholst@users.noreply.github.com>
2020-01-25 05:35:08 +0100

    make webpack dev server listen on local network (testing with real mobile devices)

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,6 +5,7 @@ process.env.NODE_ENV = "development";
 module.exports = Object.assign(commonConfig, {
     mode: "development",
     devServer: {
+        host: "0.0.0.0", // listen for requests from network
         // stats: "minimal",
         // contentBase: ".",
         historyApiFallback: true
